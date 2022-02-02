804175fe909ef638d53eec97679fb890d2baeb7b
Andrew Musselman <andrew.mussels@gmail.com>
2022-02-02 06:52:46 -0700

    Add it block expecting the frist argument of the Turn class to be a string

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -13,5 +13,8 @@ describe('Test', function() {
   });
   it('Should be an instance of Turn', function() {
     expect(turn).to.be.instanceof(Turn);
-  })
+  });
+  it('Should take a string as its first argument', function() {
+    expect(turn.arguments[0]).to.be.a('string');
+  });
 });
