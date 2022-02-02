d7fbb6ca9128000381ffc61e821f7c073202bb45
Andrew Musselman <andrew.mussels@gmail.com>
2022-02-02 07:00:06 -0700

    Remove hook from describe block

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -3,18 +3,18 @@ const expect = chai.expect;
 
 const Turn = require('../src/Turn');
 
-describe('Test', function() {
-  beforEach(function() {
-    const turn = new Turn();
-  })
+describe('Turn', function() {
 
   it('Should be a function', function() {
+    const turn = new Turn();
     expect(Turn).to.be.a('function');
   });
   it('Should be an instance of Turn', function() {
+    const turn = new Turn();
     expect(turn).to.be.instanceof(Turn);
   });
   it('Should take a string as its first argument', function() {
+    const turn = new Turn();
     expect(turn.arguments[0]).to.be.a('string');
   });
 });
