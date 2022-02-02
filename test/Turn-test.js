cfa1387383b41708af8649255069915d8b139b8c
Andrew Musselman <andrew.mussels@gmail.com>
2022-02-02 07:18:38 -0700

    Add it block expecting Turn to take a card object as its second argument

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -2,6 +2,7 @@ const chai = require('Chai');
 const expect = chai.expect;
 
 const Turn = require('../src/Turn');
+const Card = require('../src/Card');
 
 describe('Turn', function() {
 
@@ -14,7 +15,11 @@ describe('Turn', function() {
     expect(turn).to.be.instanceof(Turn);
   });
   it('Should take a string as its first argument', function() {
-    const turn = new Turn();
+    const card = new Card();
+    const turn = new Turn('guess', card);
     expect(turn.arguments[0]).to.be.a('string');
+  });
+  it('Should take a card object as its second argument', function() {
+    expect(turn.arguments[1]).to.be.an('object');
   });
 });
