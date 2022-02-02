18fb110087ca453c48093452b95fefbe4a6eca4d
Andrew Musselman <andrew.mussels@gmail.com>
2022-02-02 06:44:42 -0700

    Write it block expecting turn to instantiate a new Turn

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -4,9 +4,14 @@ const expect = chai.expect;
 const Turn = require('../src/Turn');
 
 describe('Test', function() {
+  beforEach(function() {
+    const turn = new Turn();
+  })
 
   it('Should be a function', function() {
-    const turn = new Turn();
     expect(Turn).to.be.a('function');
   });
+  it('Should be an instance of Turn', function() {
+    expect(turn).to.be.instanceof(Turn);
+  })
 });
