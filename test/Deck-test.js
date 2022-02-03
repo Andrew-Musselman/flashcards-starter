0fdf70367fc75b130aacd8a1d18aa8fee951ae29
Andrew Musselman <andrew.mussels@gmail.com>
2022-02-03 13:32:19 -0700

    Set up it blocks for test

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -15,5 +15,14 @@ describe('Deck' function() {
     card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
     card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
     deck = new Deck([card1, card2, card3]);
-  })
+  });
+  it('Should be a function', function() {
+    
+  });
+  it('Should take in cards as parameters', function() {
+
+  });
+  it('Should know how many cards are in the deck', function() {
+
+  });
 })
