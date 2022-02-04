10a23d7810a4f8ad7388ea06994fdeed48e84236
Andrew Musselman <andrew.mussels@gmail.com>
2022-02-03 18:45:22 -0700

    Add take turn method to round class

--- src/Round.js
+++ src/Round.js
@@ -1,6 +1,18 @@
+const Turn = require('../src/Turn');
+
 class Round {
   constructor(deck) {
     this.deck = deck.cards;
+    this.turns = 0
+  }
+  returnCurrentCard() {
+    let turnCount = this.turns;
+    return this.deck[turnCount]
+  }
+  takeTurn(guess) {
+    const turn = new Turn(guess, this.returnCurrentCard())
+    this.turns ++
+    return turn.giveFeedback()
   }
 }
 
