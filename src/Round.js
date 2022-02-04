64aabde7b797b80dac51c431f03e524d27e6056b
Andrew Musselman <andrew.mussels@gmail.com>
2022-02-03 18:56:00 -0700

    Add to logic to take turn method to store incorrect guesses

--- src/Round.js
+++ src/Round.js
@@ -3,16 +3,20 @@ const Turn = require('../src/Turn');
 class Round {
   constructor(deck) {
     this.deck = deck.cards;
-    this.turns = 0
+    this.turns = 0;
+    this.incorrectGuesses = [];
   }
   returnCurrentCard() {
     let turnCount = this.turns;
-    return this.deck[turnCount]
+    return this.deck[turnCount];
   }
   takeTurn(guess) {
-    const turn = new Turn(guess, this.returnCurrentCard())
-    this.turns ++
-    return turn.giveFeedback()
+    const turn = new Turn(guess, this.returnCurrentCard());
+    if(!turn.evaluateGuess()){
+    this.incorrectGuesses.push(this.returnCurrentCard().id);
+    }
+    this.turns ++;
+    return turn.giveFeedback();
   }
 }
 
